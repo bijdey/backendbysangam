5cdea9ac12830917ceeb9058d02ba2b5ea1f837b
bijdey <creativenetwork.bijay@gmail.com>
2025-03-07 11:42:49 +0530

    server setup

--- 6.http-module/Server.js
+++ 6.http-module/Server.js
@@ -1,7 +1,12 @@
-const http= require('http');
+const http = require('http');
+const port = 3000;
 
+const server = http.createServer((req, res) => {
+    console.log("Request received:", req.method, req.url); // Log request method & URL
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello from server');
+});
 
-const server = http.createServer((req,res)=>{
-
-    console.log(req,'res')
-})
+server.listen(port, () => {
+    console.log(`Server is running on port\n http://localhost:${port}`);
+});
