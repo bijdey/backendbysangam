3a1f2ef91d5f9e2ce7d38d0e8327565f79b77cfb
bijdey <creativenetwork.bijay@gmail.com>
2025-07-04 12:02:49 +0530

    made changes in the project

--- Booksapi/models/book.js
+++ Booksapi/models/book.js
@@ -4,6 +4,13 @@ const mongoose= require('mongoose')
 const bookSchema= new mongoose.Schema({
     title: {type: String, required: [true, 'Book titile is required'],
         trim:true,
-        maxLenght: [100, 'Book title cannot be more then 100 characters/'] 
+        maxLenght: [100, 'Book title cannot be more then 100 characters'] 
+    },
+      author: {type: String, required: [true, 'Book author name is required'],
+        trim:true,
+      
+    },
+    year:{
+        type: Nume 
     }
 })
