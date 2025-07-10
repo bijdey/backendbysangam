e13f42c428bdf842d1c46517efa1b0a43fc22194
bijdey <creativenetwork.bijay@gmail.com>
2025-07-10 15:33:33 +0530

    completed till 416

--- Booksapi/controllers/book-controller.js
+++ Booksapi/controllers/book-controller.js
@@ -1,21 +1,38 @@
 //importing model
 const Book= require('../models/book')
 
-const getAllBooks= async(req,res)=>{
-
-}
+const getAllBooks = async (req, res) => {
+  try {
+    const allBooks = await Book.find({});
+    
+    if (allBooks?.length > 0) {
+      res.status(200).json({
+        success: true,
+        message: 'List of all the books fetched successfully',
+        data: allBooks
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: 'No books found in the database'
+      });
+    }
+    
+  } catch (e) {
+    console.error('Error at getAllBooks:', e);
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+      error: e.message
+    });
+  }
+};
 
 
 const getSingleBookById = async (req, res) => {
   try {
-    const newBookFormData = req.body; 
-    const newlyCreatedBook = await Book.create(newBookFormData); 
-    
-    res.status(201).json({
-      success: true,
-      message: 'Book added',
-      data: newlyCreatedBook 
-    });
+   
+
   } catch (e) {
     console.error('Error in getSingleBookById:', e);
     res.status(500).json({ 
@@ -25,9 +42,25 @@ const getSingleBookById = async (req, res) => {
   }
 };
 
-const addNewBook= async(req,res)=>{
-
-}
+const addNewBook = async (req, res) => {
+  try {
+    const newBookFormData = req.body;
+    const newlyCreatedBook = await Book.create(newBookFormData);
+    if (newBookFormData) {
+      res.status(201).json({
+        success: true,
+        message: "Book added successfully",
+        data: newlyCreatedBook,
+      });
+    }
+  } catch (e) {
+    console.error('error at addNewBook', e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again",
+    });
+  }
+};
 
 
 const updateBook= async(req,res)=>{
@@ -41,3 +74,4 @@ const deleteBook= async(req,res)=>{
 
 module.exports={getAllBooks, getSingleBookById, addNewBook, updateBook, deleteBook}
 
+ 
