f639871270c714b079a6630a6b0a823aaee7e069
bijdey <creativenetwork.bijay@gmail.com>
2025-07-26 15:57:22 +0530

    838

--- nodejs-sockets/server.js
+++ nodejs-sockets/server.js
@@ -1,17 +1,39 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-const express= require('express')
-const http= require('http')
-const socketIo= require('socket.io')
+const app = express();
+const port = 3000;
 
-const app= express()
+// Create the HTTP server
+const server = http.createServer(app);
 
-//create the http server
-const server= http.createServer(app)
+// Attach socket.io to the HTTP server
+const io = socketIo(server);
+
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
+// Track connected users
+const users = new Set();
+
+io.on("connection", (socket) => {
+  console.log('A user is now connected');
+
+  // Handle when a user joins the chat
+  socket.on('join', (userName) => {
+    users.add(userName);
+
+    // Notify everyone a user joined
+    io.emit('userJoined', userName);
+
+    // Send updated user list to all clients
+    io.emit('userList', Array.from(users));
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
 
-//initiate socket.io and then attach this to t he http server
-const io= socketIo(server)
-
-app.use(express.static('public'))
-
-const users= new Set()
